2b171cf5b098140d53db8e232d6e1bf673e24cab
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2018-05-18 23:50:17 +0200

    Dynamic copyright year display

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  public currentYear: Number = new Date().getFullYear();
 
   constructor() { }
 
