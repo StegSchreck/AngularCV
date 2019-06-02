8b6ded153fc77220b33c92804c6d6d4b40d57170
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2019-06-02 14:05:48 +0200

    More tests for PDF generation - refs #31

--- src/app/pdf/pdf.component.spec.ts
+++ src/app/pdf/pdf.component.spec.ts
@@ -34,9 +34,199 @@ describe('PdfComponent', () => {
   it('should generate the PDF', () => {
     const pdf = component.generatePdfDocument(new jsPDF());
     expect(pdf).toBeDefined();
-    expect(pdf.internal.getNumberOfPages()).toBeGreaterThan(0);
+    expect(pdf.internal.getNumberOfPages()).toEqual(1);
     expect(pdf.getCreationDate('jsDate').getFullYear()).toEqual(new Date().getFullYear());
     expect(pdf.getCreationDate('jsDate').getMonth()).toEqual(new Date().getMonth());
     expect(pdf.getCreationDate('jsDate').getDate()).toEqual(new Date().getDate());
+  });
+
+  it('should generate the PDF containing data for professional experience', () => {
+    component.cvItems = [{
+      title: 'European Space Agency',
+      subtitle: 'Master of the Universe',
+      begin: '2000-01-01',
+      end: '',
+      description: 'To boldy go where no man has gone before.',
+      tags: ['space', 'flight'],
+      link: '',
+      thumbnail: '',
+    }];
+    const pdf = component.generatePdfDocument(new jsPDF());
+    expect(pdf.internal.getNumberOfPages()).toEqual(2);
+  });
+
+  it('should generate the PDF with multiple pages, if many cv items', () => {
+    component.cvItems = [{
+      title: 'European Space Agency',
+      subtitle: 'Master of the Universe',
+      begin: '2000-01-01',
+      end: '',
+      description: 'To boldy go where no man has gone before.',
+      tags: ['space', 'flight'],
+      link: '',
+      thumbnail: '',
+    }, {
+      title: 'European Space Agency',
+      subtitle: 'Master of the Universe',
+      begin: '2000-01-01',
+      end: '',
+      description: 'To boldy go where no man has gone before.',
+      tags: ['space', 'flight'],
+      link: '',
+      thumbnail: '',
+    }, {
+      title: 'European Space Agency',
+      subtitle: 'Master of the Universe',
+      begin: '2000-01-01',
+      end: '',
+      description: 'To boldy go where no man has gone before.',
+      tags: ['space', 'flight'],
+      link: '',
+      thumbnail: '',
+    }, {
+      title: 'European Space Agency',
+      subtitle: 'Master of the Universe',
+      begin: '2000-01-01',
+      end: '',
+      description: 'To boldy go where no man has gone before.',
+      tags: ['space', 'flight'],
+      link: '',
+      thumbnail: '',
+    }, {
+      title: 'European Space Agency',
+      subtitle: 'Master of the Universe',
+      begin: '2000-01-01',
+      end: '',
+      description: 'To boldy go where no man has gone before.',
+      tags: ['space', 'flight'],
+      link: '',
+      thumbnail: '',
+    }, {
+      title: 'European Space Agency',
+      subtitle: 'Master of the Universe',
+      begin: '2000-01-01',
+      end: '',
+      description: 'To boldy go where no man has gone before.',
+      tags: ['space', 'flight'],
+      link: '',
+      thumbnail: '',
+    }, {
+      title: 'European Space Agency',
+      subtitle: 'Master of the Universe',
+      begin: '2000-01-01',
+      end: '',
+      description: 'To boldy go where no man has gone before.',
+      tags: ['space', 'flight'],
+      link: '',
+      thumbnail: '',
+    }];
+    const pdf = component.generatePdfDocument(new jsPDF());
+    expect(pdf.internal.getNumberOfPages()).toEqual(3);
+  });
+
+  it('should generate the PDF containing data for education', () => {
+    component.educationItems = [{
+      title: 'Fancy University',
+      subtitle: 'PhD',
+      begin: '2000-01-01',
+      end: '',
+      description: 'doctor who?',
+      tags: ['doctor'],
+      link: '',
+      thumbnail: '',
+    }];
+    component.certificationItems = [{
+      title: 'Fancy University',
+      subtitle: 'Certificate of Awesomeness',
+      begin: '2000-01-01',
+      end: '2005-01-01',
+      description: '',
+      tags: [],
+      link: '',
+      thumbnail: '',
+    }];
+    component.languageItems = [{ title: 'English', level: '99' }];
+    const pdf = component.generatePdfDocument(new jsPDF());
+    expect(pdf.internal.getNumberOfPages()).toEqual(3);
+  });
+
+  it('should generate the PDF containing data for volunteering', () => {
+    component.volunteerItems = [{
+      title: 'Red Cross',
+      subtitle: 'Blood Donation',
+      begin: '2000-01-01',
+      end: '',
+      description: '',
+      tags: [],
+      link: '',
+      thumbnail: '',
+    }];
+    const pdf = component.generatePdfDocument(new jsPDF());
+    expect(pdf.internal.getNumberOfPages()).toEqual(2);
+  });
+
+  it('should generate the PDF containing data for publication and talks', () => {
+    component.publicationItems = [{
+      title: 'Medium article',
+      subtitle: 'Sharing my wisdom',
+      begin: '2000-01-01',
+      end: '',
+      description: '',
+      tags: [],
+      link: '',
+      thumbnail: '',
+    }];
+    component.talkItems = [{
+      title: 'TED talk',
+      subtitle: 'Blood Donation',
+      begin: 'Sharing my wisdom',
+      end: '',
+      description: '',
+      tags: [],
+      link: '',
+      thumbnail: '',
+    }];
+    const pdf = component.generatePdfDocument(new jsPDF());
+    expect(pdf.internal.getNumberOfPages()).toEqual(3);
+  });
+
+  it('should generate the PDF containing data for volunteering', () => {
+    component.volunteerItems = [{
+      title: 'Red Cross',
+      subtitle: 'Blood Donation',
+      begin: '2000-01-01',
+      end: '',
+      description: '',
+      tags: [],
+      link: '',
+      thumbnail: '',
+    }];
+    const pdf = component.generatePdfDocument(new jsPDF());
+    expect(pdf.internal.getNumberOfPages()).toEqual(2);
+  });
+
+  it('should generate the PDF containing data for projects', () => {
+    component.projectItems = [{
+      title: 'Red Cross',
+      subtitle: 'Blood Donation',
+      begin: '2000-01-01',
+      end: '',
+      description: '',
+      tags: [],
+      link: '',
+      thumbnail: '',
+    }];
+    const pdf = component.generatePdfDocument(new jsPDF());
+    expect(pdf.internal.getNumberOfPages()).toEqual(2);
+  });
+
+  it('should generate the PDF containing data for general data', () => {
+    component.generalData = {
+      name: 'Sebastian Schreck',
+      position: 'Software Engineer',
+      description: 'This is just a test description.'
+    };
+    const pdf = component.generatePdfDocument(new jsPDF());
+    expect(pdf.internal.getNumberOfPages()).toEqual(1);
   });
 });
