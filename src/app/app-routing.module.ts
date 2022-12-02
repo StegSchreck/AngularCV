53c851e6a60ee90ac558e53093668e6aeb42796e
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2022-12-02 16:46:11 +0100

    Agnular upgrade 14.2.10 -> 15.0.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,8 +25,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules,
-    relativeLinkResolution: 'legacy'
+    preloadingStrategy: PreloadAllModules
 }) ],
   exports: [ RouterModule ]
 })
