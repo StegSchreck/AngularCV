7feb5b1a96a300379ffabfba9346c9bd8125fb24
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2023-10-02 15:44:51 +0200

    feat: Show app version on About page - #1064
    Co-authored-by: Sebastian Schreck <sebastian.schreck@netlight.com>
    Co-authored-by: Sebastian Schreck <sebastian.schreck@stegschreck.de>
    Co-authored-by: Sebastian Schreck <github@stegschreck.de>
    Co-authored-by: Sebastian Schreck <sebastian.schreck@alumni.tu-berlin.de>

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { LocalizationService } from '../l10n/l10n.service';
+import packageJson from '../../../package.json';
 
 @Component({
   selector: 'app-about',
@@ -9,6 +10,7 @@ import { LocalizationService } from '../l10n/l10n.service';
 })
 export class AboutComponent implements OnInit {
   public l10n;
+  public appVersion: string = packageJson.version;
 
   constructor(
     private localizationService: LocalizationService,
